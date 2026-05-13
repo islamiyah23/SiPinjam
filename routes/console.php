d683c039da7626cfa05ea23714e3fddd9ee9de4a
Islamiyah Dinar Kinanti <islamiyahdinark@gmail.com>
2026-05-13 20:32:42 +0800

    feat(console): register new artisan commands

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+// SLA Auto-Reject: tolak otomatis peminjaman pending > 48 jam
+Schedule::command('booking:auto-reject')->hourly();
