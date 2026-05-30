d04ca34da49ebe83ea5fec20993075ea5f64513d
belpythons <zerobocilicung393@gmail.com>
2026-05-30 12:12:05 +0800

    feat(auth): implement user block verification middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
 
         $middleware->alias([
             'admin' => \App\Http\Middleware\IsAdmin::class,
+            'blocked' => \App\Http\Middleware\CheckUserBlocked::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
